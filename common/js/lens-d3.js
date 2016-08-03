751df587df99f10f44ab5afa9ab63ea4c7badf33
Don Button <donpublic@digitalgizmo.com>
2016-08-03 14:04:56 -0400

    add lens border

--- common/js/lens-d3.js
+++ common/js/lens-d3.js
@@ -1,9 +1,19 @@
 // magic lens JS
+var lw = 300; // lens width
+var lh = 200;
+var lsx = 160; // lens start x
+var lsy = 25; 
+
+
+// drag border and clip with it
 var drag = d3.drag()
     .on("drag", function() {
         var clippy = d3.select('#clip rect');
         clippy.attr('x', +clippy.attr('x') + d3.event.dx);
         clippy.attr('y', +clippy.attr('y') + d3.event.dy);
+        var border = d3.select('#lens-border');
+        border.attr('x', +border.attr('x') + d3.event.dx);
+        border.attr('y', +border.attr('y') + d3.event.dy);
     });
 
 /*
@@ -15,16 +25,51 @@ var svg = d3.select("svg")
     .attr('transform', 'translate('+margin+','+margin+')');
 */
 
+// add border - before lens
+d3.select("svg g").append("svg:rect")
+    .attr("id", "lens-border")
+    .attr('x', lsx)
+    .attr('y', lsy)
+    .attr('width', lw + 2)
+    .attr('height', lh + 2)
+    .attr('rx', 15)
+    .attr('ry', 15)
+    .style("fill-opacity", 0)
+    .style("stroke", "#ACA9A4")
+    .style("stroke-width", 4)
+    .style('cursor', 'move')
+    .call(drag);
+    ;
+
+
+// add lens clip
 var clip = d3.select("svg g").append("svg:clipPath")
     .attr("id", "clip")
     .append("svg:rect")
-    .attr('x', 160)
-    .attr('y', 25)
+    .attr('x', lsx)
+    .attr('y', lsy)
     .attr('rx', 15)
     .attr('ry', 15)
-    .attr('width', 300)
-    .attr('height', 200)
+    .attr('width', lw)
+    .attr('height', lh)
     ;
+
+/*
+// test adding sizer
+//var clip = d3.select("svg g").append("svg:clipSizer")
+d3.select("svg g").append("svg:clipSizer")
+    .attr("id", "sizer")
+    .append("svg:rect")
+    .attr('x', 540)
+    .attr('y', 180)
+    .attr('width', 20)
+    .attr('height', 20)
+    .style("fill", "orange")
+    .style("stroke", "black")
+    ;
+*/
+
+
 /*
 // background rect -- superceeded by actual image
 svg.append('svg:rect')
@@ -37,10 +82,12 @@ svg.append('svg:rect')
 
     // adding
     .attr("id", "doc-image")
-
 */
 
+
+// add lens and drag to foreground image
 d3.select("#doc-image")
     .attr("clip-path", function(d,i) { return "url(#clip)"; })
-    .call(drag);
+    // drag now needs to be on border object since that's in the foreground
+    //.call(drag);
 
